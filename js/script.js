34aee934d73d445339c37e0372b1b306ac6b5544
Murilo Silva <107772420+xmurilo@users.noreply.github.com>
2023-04-11 15:36:26 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ let searchPokemon = 1;
 const fetchPokemon  = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     
-    if (APIResponse.status = 200){
+    if (APIResponse.status == 200){
         const data = await APIResponse.json();
         return data;
     }
